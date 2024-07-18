d91273b7a9df5ad796e8aca884ddf5b19c29a8b4
Nicolas <nicolaspicon98@outlook.com>
2024-07-18 00:48:56 -0500

    this is another test

--- src/main/java/com/addiis/core/gestionlogistica/services/DistribucionService.java
+++
@@ -1,7 +0,0 @@
-package com.addiis.core.gestionlogistica.services;
-
-// import com.addiis.core.gestionlogistica.domain.dto.ResponseConsultaMaterialDTO;
-
-public interface DistribucionService {
-    // ResponseConsultaMaterialDTO consultaDistribucion(String sku);
-}
