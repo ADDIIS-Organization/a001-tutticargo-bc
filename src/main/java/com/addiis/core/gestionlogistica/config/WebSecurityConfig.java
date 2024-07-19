df5246dd38007262fda38344540eda61e0608f22
Nicolas <nicolaspicon98@outlook.com>
2024-07-19 11:08:47 -0500

    adds cors config

--- src/main/java/com/addiis/core/gestionlogistica/config/WebSecurityConfig.java
+++ src/main/java/com/addiis/core/gestionlogistica/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 @Configuration
@@ -39,5 +41,15 @@ class WebSecurityConfig {
                                 .addFilterAfter(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
 
                 return http.build();
+        }
+
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+                return new WebMvcConfigurer() {
+                        @Override
+                        public void addCorsMappings(CorsRegistry registry) {
+                                registry.addMapping("/**").allowedOrigins("*");
+                        }
+                };
         }
 }
