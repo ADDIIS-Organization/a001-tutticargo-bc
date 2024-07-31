7e881e13fff05f6bbf9a587a9ea0240af9e6c599
juanregino <juanreginopenagos@gmail.com>
2024-07-30 22:28:04 -0500

    add getDispatches by date

--- src/main/java/com/addiis/core/gestionlogistica/services/dispatch/DispatchService.java
+++ src/main/java/com/addiis/core/gestionlogistica/services/dispatch/DispatchService.java
@@ -1,9 +1,14 @@
 package com.addiis.core.gestionlogistica.services.dispatch;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
 import com.addiis.core.gestionlogistica.domain.dto.request.DispatchRequest;
 import com.addiis.core.gestionlogistica.domain.dto.response.DispatchResponse;
 import com.addiis.core.gestionlogistica.services.CRUDService;
 
 public interface DispatchService extends CRUDService<DispatchRequest, DispatchResponse, Long> {
-  
+  Page<DispatchResponse> findDispatchesByDate(LocalDate dispatchDate , int page, int size);
 }
